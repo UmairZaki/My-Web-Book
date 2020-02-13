2b3842d2f0174dd0b4f7fd4f4c20996f0d6dda99
Umair Zaki <umairzakicnbc@gmail.com>
2020-02-13 10:27:33 -0500

    My Web Book

---
+++ src/components/Gallery.js
@@ -0,0 +1,79 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Carousel, { Modal, ModalGateway } from "react-images";
+
+class Gallery extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            lightboxIsOpen: false,
+            selectedIndex: 0
+        };
+        
+        this.toggleLightbox = this.toggleLightbox.bind(this);
+    }
+    toggleLightbox(selectedIndex) {
+        this.setState(state => ({
+            lightboxIsOpen: !state.lightboxIsOpen,
+            selectedIndex
+        }));
+    }
+    renderGallery (images) {
+        if (!images) return;
+
+        const gallery = images.map((obj, i) => {
+            return (
+                <article className="6u 12u$(xsmall) work-item c" key={i}>
+                    <a
+                        className="image fit thumb"
+                        href={obj.source}
+                        onClick={e => {
+                            e.preventDefault();
+                            this.toggleLightbox(i);
+                        }}
+                    >
+                        <img src={obj.thumbnail} />
+                    </a>
+                    <ul className="actions align-center b">
+                            <li><a href={obj.caption} target="_blank">Visit Site</a></li>
+                        </ul>
+                    
+                    
+                    
+                </article>
+            );
+        });
+
+        return (
+            <div className="row">
+                {gallery}
+            </div>
+        );
+    }
+    render () {
+        const { images } = this.props;
+        const { selectedIndex, lightboxIsOpen } = this.state;
+
+        return (
+            <div>
+                {this.renderGallery(images)}
+                <ModalGateway>
+                    {lightboxIsOpen && (
+                        <Modal onClose={this.toggleLightbox}>
+                            <Carousel currentIndex={selectedIndex} views={images} />
+                        </Modal>
+                        
+                    )}
+                </ModalGateway>
+            </div>
+        );
+    }
+}
+
+Gallery.displayName = 'Gallery';
+Gallery.propTypes = {
+    images: PropTypes.array
+};
+
+export default Gallery;
---
+++ src/pages/index.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import Layout from '../components/layout'
+// import Lightbox from 'react-images'
+import Gallery from '../components/Gallery'
+
+import thumb01 from '../assets/images/thumbs/1.jpg'
+import thumb02 from '../assets/images/thumbs/2.jpg'
+import thumb03 from '../assets/images/thumbs/3.jpg'
+import thumb04 from '../assets/images/thumbs/4.jpg'
+import thumb05 from '../assets/images/thumbs/5.jpg'
+import thumb06 from '../assets/images/thumbs/6.jpg'
+import thumb07 from '../assets/images/thumbs/7.jpg'
+import thumb08 from '../assets/images/thumbs/8.jpg'
+
+import full01 from '../assets/images/fulls/1.jpg'
+import full02 from '../assets/images/fulls/2.jpg'
+import full03 from '../assets/images/fulls/3.jpg'
+import full04 from '../assets/images/fulls/4.jpg'
+import full05 from '../assets/images/fulls/5.jpg'
+import full06 from '../assets/images/fulls/6.jpg'
+import full07 from '../assets/images/fulls/7.jpg'
+import full08 from '../assets/images/fulls/8.jpg'
+
+const DEFAULT_IMAGES = [
+    { id: '1', source: full01, thumbnail: thumb01, caption: 'https://umairportfolio.netlify.com/'},
+    { id: '2', source: full02, thumbnail: thumb02, caption: 'https://umairzaki-new.netlify.com/'},
+    { id: '3', source: full03, thumbnail: thumb03, caption: 'https://umair-zaki.netlify.com/'},
+    { id: '4', source: full04, thumbnail: thumb04, caption: 'https://umairzakiportfolio.netlify.com/'},
+    { id: '5', source: full05, thumbnail: thumb05, caption: 'https://umairzakicv.netlify.com/'},
+    { id: '6', source: full06, thumbnail: thumb06, caption: 'https://umairzakiresume.netlify.com/'},
+    { id: '7', source: full07, thumbnail: thumb07, caption: 'https://umairzaki.netlify.com/'},
+    { id: '8', source: full08, thumbnail: thumb08, caption: 'https://umairnew.netlify.com/'}
+];
+
+class HomeIndex extends React.Component {
+
+    render() {
+        const siteTitle = "Umair Zaki | Shocase"
+        const siteDescription = "My Web Book"
+
+        return (
+            <Layout>
+                <Helmet>
+                        <title>{siteTitle}</title>
+                        <meta name="description" content={siteDescription} />
+                </Helmet>
+
+                <div id="main" className="a">
+
+                    
+
+                    <section id="two ">
+                        <h2>My Web Book</h2>
+
+                        <Gallery images={DEFAULT_IMAGES.map(({ id, source, thumbnail, caption, description, url }) => ({
+                            source,
+                            thumbnail,
+                            caption,
+                            description,
+                            url
+                        }))} />
+
+                        
+                    </section>
+
+                    
+
+                </div>
+
+            </Layout>
+        )
+    }
+}
+
+export default HomeIndex
